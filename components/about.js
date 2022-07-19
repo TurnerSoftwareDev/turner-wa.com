99868f304baa430b296dab7cdfaf048c6737981d
Eric <eric@ericturner.dev>
2022-07-19 15:47:02 -0700

    Make years of service dynamic

--- components/about.js
+++ components/about.js
@@ -13,7 +13,7 @@ export default function About({className}) {
 
                 <p className="ml-4 w-5/6">
                     Richard is a licensed Professional Electrical Engineer focusing primarily on Electrical and
-                    Instrumentation Systems with over 35 years of experience. His responsibilities have
+                    Instrumentation Systems with {new Date().getFullYear()-1974} years of experience. His responsibilities have
                     included overall project management, often in a multiple engineering team membership environment,
                     working from concept through construction and start-up. Clients include industrial facilities, oil
                     and gas pipelines, refineries, distribution facilities, public facilities, educational campuses, and
