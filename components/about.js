ec6e096a51a03144e067717dffca86f9905217e2
Eric <eric@ericturner.dev>
2022-07-20 09:39:21 -0700

    Make text wrap about image in About

--- components/about.js
+++ components/about.js
@@ -4,21 +4,27 @@ import Image from 'next/image';
 export default function About({className}) {
     return (
         <section className={classNames('about flex flex-col items-center p-4 md:p-8', className)}>
-            <div className="flex flex-row items-center max-w-2xl">
-                <Image className="rounded-lg"
-                       src="/images/richard_400.jpg"
-                       alt="Richard Turner portrait"
-                       width={400}
-                       height={517}/>
+            <div className="flex flex-row items-center max-w-3xl">
+                <div>
+                    <div className="float-left relative">
+                        <div className="w-32 h-32 md:w-40 md:h-40 lg:w-52 lg:h-52 xl:w-64 xl:h-64">
+                            <Image className="rounded-lg"
+                                   src="/images/richard_400.jpg"
+                                   alt="Richard Turner portrait"
+                                   layout="fill"
+                                   objectFit="scale-down"
+                            />
+                        </div>
+                    </div>
 
-                <p className="ml-4 w-5/6">
                     Richard is a licensed Professional Electrical Engineer focusing primarily on Electrical and
-                    Instrumentation Systems with {new Date().getFullYear()-1974} years of experience. His responsibilities have
+                    Instrumentation Systems with {new Date().getFullYear() - 1974} years of experience. His
+                    responsibilities have
                     included overall project management, often in a multiple engineering team membership environment,
                     working from concept through construction and start-up. Clients include industrial facilities, oil
                     and gas pipelines, refineries, distribution facilities, public facilities, educational campuses, and
                     municipal wastewater systems.
-                </p>
+                </div>
             </div>
         </section>
     )
